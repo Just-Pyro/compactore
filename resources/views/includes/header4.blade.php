18c076f52d54faf7cfd131a7f39bb941d98c7f6f
Just-Pyro <nacuapraiseemmanuel@gmail.com>
2023-11-02 22:03:53 +0800

    swapme front-end started

--- resources/views/includes/header4.blade.php
+++ resources/views/includes/header4.blade.php
@@ -1,11 +1,11 @@
-<header class="bg-dark" id="header2">
+<header class="bg-dark fixed-top" id="header2">
     <div class="container-fluid d-flex flex-wrap mx-0">
       <span class="d-flex align-items-center mx-1 text-white"><i class="fas fa-phone-square-alt pe-1"></i>(+000)000-0000</span>
       <span class="d-flex align-items-center mx-1 text-white"><i class="fas fa-envelope-square pe-1"></i>compactore@email.com</span>
       <a href="{{ route('register') }}" class="d-flex align-items-center ms-auto text-white"  id="logout">logout</a>
     </div>
   </header>
-  <nav id="head1" class="navbar navbar-expand-lg bg-light navbar-light shadow">
+  <nav id="head1" class="navbar navbar-expand-lg bg-light navbar-light shadow" style="position: fixed; right: 0; left: 0; top: 24px; z-index: 1;">
     <div class="container">
         <a id="title" href="{{ url('/ecommerce') }}" class="navbar-brand">COMPACTORE
           <img src="{{ URL('images/swapme.png') }}" alt="trade" id="trade" style="height: 28px; object-fit:contain;">
--- resources/views/trading/swapme.blade.php
+++ resources/views/trading/swapme.blade.php
@@ -6,11 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Swap Me | Compactore</title>
     <link rel="stylesheet" href="/customcss/ecommerce.css">
+    <link rel="stylesheet" href="/customcss/swapme.css">
     @include('includes/header1')
 </head>
 <body>
     @include('includes/header4')
-    this is swap me
+    <div class="container" style="margin-top: 100px; position: relative;">
+        {{-- post something --}}
+        {{-- <div class="card sticky-div border rounded mt-5" style="height: 100px;">
+            <div class="card-body">
+
+            </div>
+        </div> --}}
+
+        <div class="card border rounded mt-3 shadow-sm" style="height: 100px;">
+            <div class="card-body">
+                this is a post
+            </div>
+        </div>
+
+
+        {{-- Post Item --}}
+        <div class="border shadow-sm rounded p-2" id="postBtn">
+            <img src="{{ url('images/otherIcons/writePostIcon.png') }}" alt="postIcon" id="postIcon">
+            Post Item
+        </div>
+    </div>
     @include('includes/footer1')
     <script src="js/swapme.js"></script>
 </body>
