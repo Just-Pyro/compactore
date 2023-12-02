a2863d37dbbe97221db4ea0a60b93206604bd513
Just-Pyro <nacuapraiseemmanuel@gmail.com>
2023-12-02 11:46:58 +0800

    addition changes

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -8,24 +8,6 @@ use Curl;
 
 class CheckoutController extends Controller
 {
-    // public function createSession()
-    // {
-    //     $apiKey = env('PAYMONGO_API_KEY');
-
-    //     $client = new Client();
-
-    //     $response = $client->request('POST', 'https://api.paymongo.com/v1/checkout_sessions', [
-    //         'body' => '{"data":{"attributes":{"send_email_receipt":false,"show_description":true,"show_line_items":true}}}',
-    //         'headers' => [
-    //             'Content-Type' => 'application/json',
-    //             'accept' => 'application/json',
-    //             'Authorization' => 'Bearer ' . $apiKey,
-    //         ],
-    //     ]);
-
-    //     // Handle the response as needed, e.g., return it or further processing
-    //     return $response->getBody();
-    // }
 
     public function placeOrder(Request $request){
         // Validate the form data
@@ -37,10 +19,10 @@ class CheckoutController extends Controller
         $totalAmount = $request->totalPrice*100;
         // Process the order based on the selected payment method
         $paymentMethod = $request->paymentMethod;
-
+        $itemName = "";
         $productName = $request->productName;
         foreach($productName as $name){
-            $itemName = $name." | ";
+            $itemName .= $name." || ";
         }
 
         if ($paymentMethod === 'Gcash') {
@@ -61,8 +43,8 @@ class CheckoutController extends Controller
                             // 'card',
                             'gcash',
                         ],
-                        'success_url' => 'http:://localhost:8000/checkout/success',
-                        'cancel_url' => 'http:://localhost:8000/checkout/success',
+                        'success_url' => 'http://127.0.0.1:8000/checkout/success',
+                        'cancel_url' => 'http://127.0.0.1:8000/checkout',
                         // 'description' => 'text'
                     ],
                 ]
@@ -81,81 +63,8 @@ class CheckoutController extends Controller
             return redirect()->to($response->data->attributes->checkout_url);
         }
 
-        // For CoD or other payment methods, update the order status directly
-
-        // Save the order to the database
-        // ...
-
-        // Return a response
-        return response()->json(['message' => 'Order placed successfully']);
+        return view('checkout.success');
     }
-
-    // public function placeOrder(Request $request)
-    // {
-    //     // Validate the form data
-    //     $request->validate([
-    //         'paymentMethod' => 'required|in:CoD,Gcash',
-    //         'totalPrice' => 'required|numeric',
-    //         // Add additional validation rules if needed
-    //     ]);
-
-    //     $totalAmount = $request->input('totalPrice');
-    //     $paymentMethod = $request->input('paymentMethod');
-
-    //     if ($paymentMethod === 'Gcash') {
-    //         // Create a payment session on PayMongo
-    //         $paymentSessionId = $this->createPaymentSession($totalAmount);
-
-    //         // Redirect to PayMongo hosted checkout page
-    //         return redirect()->away("https://checkout.paymongo.com/{$paymentSessionId}");
-    //     }
-
-    //     // For CoD or other payment methods, update the order status directly
-    //     // Save the order to the database
-    //     // ...
-
-    //     // Return a response
-    //     return response()->json(['message' => 'Order placed successfully']);
-    // }
-
-    // protected function createPaymentSession($totalAmount)
-    // {
-    //     // Use Guzzle or any HTTP client to create a payment session on PayMongo
-    //     $client = new Client();
-    //     $paymongoSecretKey = env('PAYMONGO_SECRET_KEY');
-
-    //     $response = $client->post('https://api.paymongo.com/v2/payment_sessions', [
-    //         'headers' => [
-    //             'Content-Type' => 'application/json',
-    //             'Authorization' => 'Bearer ' . $paymongoSecretKey,
-    //         ],
-    //         'json' => [
-    //             'data' => [
-    //                 'attributes' => [
-    //                     'amount' => $totalAmount,
-    //                     'payment_method_allowed' => ['Gcash'], // Update with the allowed payment methods
-    //                     'redirect' => [
-    //                         'success' => route('checkout.success'),
-    //                         'failed' => route('checkout.failed'),
-    //                     ],
-    //                 ],
-    //             ],
-    //         ],
-    //     ]);
-
-    //     $responseData = json_decode($response->getBody(), true);
-
-    //     // Check if the response contains the session ID
-    //     if (isset($responseData['data']['id'])) {
-    //         return $responseData['data']['id'];
-    //     }
-
-    //     // Handle the case where the session ID is not available
-    //     // You might want to log an error or throw an exception
-    //     // ...
-
-    //     return null;
-    // }
 
     public function checkoutSuccess()
     {
@@ -168,8 +77,8 @@ class CheckoutController extends Controller
                             ->withHeader('Authorization: Basic c2tfdGVzdF95ZGVVNkxhaFpOcjR6and3OU5QOWlVdzI6')
                             ->asJson()
                             ->get();
-        dd($response);
-        // return view('checkout.success');
+        // dd($response);
+        return view('checkout.success');
     }
 
     public function checkoutFailed()
--- resources/views/checkout/success.blade.php
+++ resources/views/checkout/success.blade.php
@@ -1,1 +1,23 @@
-success
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/customcss/ecommerce.css">
+    <title>Document</title>
+    @include('includes/header1')
+</head>
+<body>
+    @include('includes/header2')
+    <div class="container">
+        <div class="row mt-2 bg-white rounded">
+            <h4 class="my-5 text-center fw-medium">Product Successfully Placed!</h4>
+        </div>
+        <div class="row mt-2 bg-white rounded">
+            <a href="#" class="btn btn-success">Go back</a>
+        </div>
+    </div>
+    @include('includes/footer1')
+</body>
+</html>
